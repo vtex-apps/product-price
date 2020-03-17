23a24f9d89bd5e2aef63c488bea59fc0abe69df8
iaron <iaron.araujo@vtex.com.br>
2020-03-17 16:40:02 -0300

    Added `SellingPrice`

---
+++ react/SellingPrice.tsx
@@ -0,0 +1,64 @@
+import React, { useContext } from 'react'
+import { defineMessages } from 'react-intl'
+import { ProductContext } from 'vtex.product-context'
+import { FormattedCurrency } from 'vtex.format-currency'
+import { useCssHandles } from 'vtex.css-handles'
+import { IOMessageWithMarkers } from 'vtex.native-types'
+
+import { StorefrontFC } from './types'
+
+interface Props {
+  message: string
+  markers: string[]
+}
+
+const CSS_HANDLES = ['sellingPrice', 'sellingPriceValue']
+
+const SellingPrice: StorefrontFC<Props> = props => {
+  const { message, markers } = props
+  const handles = useCssHandles(CSS_HANDLES)
+  const { selectedItem } = useContext(ProductContext)
+
+  if (!selectedItem) {
+    return null
+  }
+
+  const commertialOffer = selectedItem?.sellers[0]?.commertialOffer
+  if (!commertialOffer) {
+    return null
+  }
+
+  const sellingPriceValue = commertialOffer.Price
+
+  return (
+    <div className={handles.sellingPrice}>
+      <IOMessageWithMarkers
+        message={message}
+        markers={markers}
+        handleBase="sellingPrice"
+        values={{
+          sellingPriceValue: (
+            <span className={handles.sellingPriceValue}>
+              <FormattedCurrency value={sellingPriceValue} />
+            </span>
+          ),
+        }}
+      />
+    </div>
+  )
+}
+
+const messages = defineMessages({
+  title: {
+    id: 'admin/selling-price.title',
+  },
+  description: {
+    id: 'admin/selling-price.description',
+  },
+})
+
+SellingPrice.schema = {
+  title: messages.title.id,
+}
+
+export default SellingPrice
