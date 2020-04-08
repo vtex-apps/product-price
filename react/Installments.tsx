ebdbccc8dbab6911dd761ae9c6a576d5f2cede77
iaron <iaron.araujo@vtex.com.br>
2020-04-08 13:28:33 -0300

    Fix equals check

--- react/Installments.tsx
+++ react/Installments.tsx
@@ -28,7 +28,7 @@ const Installments: StorefrontFC<BasicPriceProps> = props => {
   const { selectedItem } = useContext(ProductContext)
 
   const commercialOffer = selectedItem?.sellers[0]?.commertialOffer
-  if (commercialOffer?.Installments?.length == 0) {
+  if (commercialOffer?.Installments?.length === 0) {
     return null
   }
 
