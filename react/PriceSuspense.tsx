366d33d4dd8ae965545e570b4d0111a1bbeae408
Hiago Lucas Cardeal <hcardealdemelo@gmail.com>
2020-10-22 10:25:11 -0300

    keep price suspense height

--- react/PriceSuspense.tsx
+++ react/PriceSuspense.tsx
@@ -1,13 +1,35 @@
 import React from 'react'
+import { useCssHandles } from 'vtex.css-handles'
 import { useProductSummary } from 'vtex.product-summary-context/ProductSummaryContext'
 
 import PriceLoadingSpinner from './components/PriceLoadingSpinner'
 import { StorefrontFC } from './types'
 
+const CSS_HANDLES = [
+  'priceSuspense',
+  'priceSuspenseLoadingWrapper',
+  'priceSuspenseCotentWrapper',
+]
+
 const PriceSuspense: StorefrontFC = ({ children }) => {
   const { isPriceLoading } = useProductSummary()
+  const handles = useCssHandles(CSS_HANDLES)
 
-  return <>{isPriceLoading ? <PriceLoadingSpinner /> : children}</>
+  return (
+    <div className={`relative ${handles.priceSuspense}`}>
+      {isPriceLoading && (
+        <div className={`absolute ${handles.priceSuspenseLoadingWrapper}`}>
+          <PriceLoadingSpinner />
+        </div>
+      )}
+      <div
+        className={handles.priceSuspenseCotentWrapper}
+        style={{ visibility: isPriceLoading ? 'hidden' : 'visible' }}
+      >
+        {children}
+      </div>
+    </div>
+  )
 }
 
 export default PriceSuspense
