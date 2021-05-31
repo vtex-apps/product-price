8f6b62674a61bc7323a617c741d21cc772dcdce1
victorhmp <victor2142@gmail.com>
2021-05-31 17:57:18 -0300

    Rename pickInstallments to pickInstallmentsList and add new functions to the module

--- react/modules/pickInstallments.ts
+++ react/modules/pickInstallments.ts
@@ -3,8 +3,8 @@ import { ProductTypes } from 'vtex.product-context'
 type ClusterBy = keyof ProductTypes.Installment
 
 /**
- * Pick which installments should be used, first it cluster all installments
- * by the value of clusterBy, then pick the cluster with the biggest amount of
+ * Pick which installments should be used. First it cluster all installments
+ * by the value of clusterBy, then picks the cluster with the biggest amount of
  * installments options and then return this list sorted by the amount of installments
  * @param installmentsList All installments
  * @param clusterBy
@@ -15,7 +15,10 @@ export default function pickInstallmentsList(
 ) {
   const clusteredInstallments = clusterInstallments(installmentsList, clusterBy)
 
-  const pickedInstallments = pickMaxOption(clusteredInstallments, clusterBy)
+  const pickedInstallments = pickMaxOptionCount(
+    clusteredInstallments,
+    clusterBy
+  )
 
   return pickedInstallments.sort(
     (a, b) => a.NumberOfInstallments - b.NumberOfInstallments
@@ -46,12 +49,12 @@ export function clusterInstallments(
 
 /**
  * Pick the cluster with the biggest amount of options, if there are multiple
- * clusters witht eh biggest amount it will pick the one that has a installment
+ * clusters with the biggest amount it will pick the one that has a installment
  * with the biggest NumberOfInstallments of all options
  * @param clusteredInstallments
  * @param clusterBy
  */
-function pickMaxOption(
+function pickMaxOptionCount(
   clusteredInstallments: Record<string, ProductTypes.Installment[]>,
   clusterBy: ClusterBy
 ) {
@@ -91,4 +94,105 @@ function pickMaxOption(
   }
 
   return clusteredInstallments[biggestInstallmentsOptionKey]
+}
+
+function applyFiltersToInstallmentsList(
+  installmentsList: ProductTypes.Installment[],
+  filteringRules: {
+    paymentSystemName?: string
+    installmentsQuantity?: number
+  }
+) {
+  let filteredInstallmentsList = installmentsList
+
+  if (filteringRules?.paymentSystemName) {
+    filteredInstallmentsList = filteredInstallmentsList.filter(
+      installmentsOption =>
+        installmentsOption.PaymentSystemName ===
+        filteringRules?.paymentSystemName
+    )
+  }
+
+  if (filteringRules?.installmentsQuantity) {
+    filteredInstallmentsList = filteredInstallmentsList.filter(
+      installmentsOption =>
+        installmentsOption.NumberOfInstallments ===
+        filteringRules?.installmentsQuantity
+    )
+  }
+
+  return filteredInstallmentsList
+}
+
+export function pickMaxInstallmentsOption(
+  installmentsList: ProductTypes.Installment[],
+  filteringRules?: {
+    paymentSystemName?: string
+    installmentsQuantity?: number
+  }
+) {
+  let filteredInstallmentsList = installmentsList
+
+  if (filteringRules) {
+    filteredInstallmentsList = applyFiltersToInstallmentsList(
+      installmentsList,
+      filteringRules
+    )
+  }
+
+  let [maxInstallmentOption] = filteredInstallmentsList
+
+  installmentsList.forEach(installmentOption => {
+    if (
+      installmentOption.NumberOfInstallments >
+      maxInstallmentOption.NumberOfInstallments
+    ) {
+      maxInstallmentOption = installmentOption
+    }
+  })
+
+  return maxInstallmentOption
+}
+
+export function pickMaxInstallmentsOptionWithNoInterest(
+  installmentsList: ProductTypes.Installment[],
+  filteringRules?: {
+    paymentSystemName?: string
+    installmentsQuantity?: number
+  }
+) {
+  let filteredInstallmentsList = installmentsList
+
+  if (filteringRules) {
+    filteredInstallmentsList = applyFiltersToInstallmentsList(
+      installmentsList,
+      filteringRules
+    )
+  }
+
+  const firstNoInterestOptionIndex = filteredInstallmentsList.findIndex(
+    option => option.InterestRate === 0
+  )
+
+  // There isn't a no-interest option
+  if (firstNoInterestOptionIndex < 0) {
+    return pickMaxInstallmentsOption(filteredInstallmentsList)
+  }
+
+  let maxWithNoInterest = filteredInstallmentsList[firstNoInterestOptionIndex]
+
+  for (
+    let index = firstNoInterestOptionIndex;
+    index < filteredInstallmentsList.length;
+    index++
+  ) {
+    if (
+      filteredInstallmentsList[index].NumberOfInstallments >
+      maxWithNoInterest.NumberOfInstallments
+    ) {
+      maxWithNoInterest = filteredInstallmentsList[index]
+    }
+  }
+
+  return maxWithNoInterest
 }
