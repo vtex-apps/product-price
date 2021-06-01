4eb96aaa9fc5850655d4f84d8821a08dac53b6bd
victorhmp <victor2142@gmail.com>
2021-06-01 09:56:14 -0300

    Add tests for new functions in pickInstallments module

--- react/__tests__/pickInstallments.test.ts
+++ react/__tests__/pickInstallments.test.ts
@@ -4,10 +4,13 @@ import {
   installmentsListMastercardMax,
 } from 'installments-list-mock'
 
-import pickInstallmentsList from '../modules/pickInstallments'
+import pickInstallmentsList, {
+  pickMaxInstallmentsOption,
+  pickMaxInstallmentsOptionWithNoInterest,
+} from '../modules/pickInstallments'
 
-describe('pickInstallments', () => {
-  it('should pick installments of the paymentmethod with the biggest amount', () => {
+describe('pickInstallmentsList', () => {
+  it('should pick installments of the payment method with the biggest amount of options', () => {
     const pickedInstallments = pickInstallmentsList(
       installmentsList,
       'PaymentSystemName'
@@ -38,5 +41,70 @@ describe('pickInstallments', () => {
     }
 
     expect(pickedInstallments[index].NumberOfInstallments).toBe(15)
+  })
+})
+
+describe('pickMaxInstallmentsOption', () => {
+  it('should pick the installments plan with the highest NumberOfInstallments', () => {
+    const pickedInstallmentsPlan = pickMaxInstallmentsOption(
+      installmentsListMastercardMax
+    )
+
+    expect(pickedInstallmentsPlan.NumberOfInstallments).toBe(15)
+    expect(pickedInstallmentsPlan.PaymentSystemName).toBe('Mastercard')
+  })
+
+  it('should pick the installments plan with the highest NumberOfInstallments, from a certain payment system', () => {
+    const pickedInstallmentsPlan = pickMaxInstallmentsOption(installmentsList, {
+      paymentSystemName: 'Customer Credit',
+    })
+
+    expect(pickedInstallmentsPlan.NumberOfInstallments).toBe(3)
+    expect(pickedInstallmentsPlan.PaymentSystemName).toBe('Customer Credit')
+  })
+
+  it('should pick an installments plan with a certain number of installments', () => {
+    const pickedInstallmentsPlan = pickMaxInstallmentsOption(
+      installmentsListMastercardMax,
+      {
+        installmentsQuantity: 14,
+      }
+    )
+
+    expect(pickedInstallmentsPlan.NumberOfInstallments).toBe(14)
+  })
+})
+
+describe('pickMaxInstallmentsOptionWithNoInterest', () => {
+  it('should pick the installments plan with the highest NumberOfInstallments and no interest', () => {
+    const pickedInstallmentsPlan = pickMaxInstallmentsOptionWithNoInterest(
+      installmentsListMastercardMax
+    )
+
+    expect(pickedInstallmentsPlan.NumberOfInstallments).toBe(14)
+    expect(pickedInstallmentsPlan.PaymentSystemName).toBe('Visa')
+    expect(pickedInstallmentsPlan.InterestRate).toBe(0)
+  })
+
+  it('should pick the installments plan with the second highest NumberOfInstallments and no interest, due to filtering rules', () => {
+    const pickedInstallmentsPlan = pickMaxInstallmentsOptionWithNoInterest(
+      installmentsList,
+      {
+        paymentSystemName: 'Customer Credit',
+      }
+    )
+
+    expect(pickedInstallmentsPlan.NumberOfInstallments).toBe(2)
+    expect(pickedInstallmentsPlan.PaymentSystemName).toBe('Customer Credit')
+    expect(pickedInstallmentsPlan.InterestRate).toBe(0)
+  })
+
+  it("should pick the installments plan with the highest NumberOfInstallments overall, if there isn't an option with no interest", () => {
+    const pickedInstallmentsPlan = pickMaxInstallmentsOptionWithNoInterest(
+      visaInstallments
+    )
+
+    expect(pickedInstallmentsPlan.NumberOfInstallments).toBe(4)
+    expect(pickedInstallmentsPlan.PaymentSystemName).toBe('Visa')
   })
 })
